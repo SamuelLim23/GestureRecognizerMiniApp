d0fbf7f04ba00831cf68d6c789500913e50cf7ec
Samuel Lim <samuel.lim23@d155.org>
2022-09-26 08:50:24 -0500

    Initial commit

--- GestureRecognizerMiniApp/ViewController.swift
+++ GestureRecognizerMiniApp/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cursorImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            let location = sender.location(in: view)
+            cursorImage.center.x = location.x
+            cursorImage.center.y = location.y
+            }
+    }
+    
+    @IBAction func handlePan(_ gesture: UIPanGestureRecognizer) {
+        // 1
+        let translation = gesture.translation(in: view)
 
+        // 2
+        guard let gestureView = gesture.view else {
+          return
+        }
+
+        gestureView.center = CGPoint(
+          x: gestureView.center.x + translation.x,
+          y: gestureView.center.y + translation.y
+        )
+
+        // 3
+        gesture.setTranslation(.zero, in: view)
+      }
 }
-
